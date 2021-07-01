2249b3f8b9740bf3e648930ea4601adbb970fe75
django <sashchystyh@yandex.ru>
2021-07-01 22:54:13 +0300

    feat: added bootstrap4

--- Webcheckfuel/settings.py
+++ Webcheckfuel/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bootstrap4",
     "parsing.apps.ParsingConfig",
     "main.apps.MainConfig",
     "account.apps.AccountConfig",
